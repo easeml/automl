6800219a922f0daec31ac12493b05eb5069149ee
Leonel Aguilar <leonel.aguilar.m@gmail.com>
2020-07-02 18:53:09 +0200

    Cleaning up

--- build_and_install_images.sh
+++ build_and_install_images.sh
@@ -13,9 +13,8 @@
 #    done
 #done
 
-DUSER="easeml"
-#config_file=$HOME/.easeml/config.yaml
-config_file=$HOME/snap/easeml/x1/.easeml/config.yaml
+config_file=$HOME/.easeml/config.yaml
+#config_file=$HOME/snap/easeml/x1/.easeml/config.yaml
 
 for t in "optimizer"
 do
@@ -23,26 +22,26 @@ do
     do
 	echo "Install module [" $t "]: " $d
 	cd ./modules/${t}s/$d
-	docker build -t $DUSER/$d .
-	#docker push $DUSER/$d
+	docker build -t $d .
 	cd ..
-	cmd="easeml create module --type $t --source upload --source-address $DUSER/$d --id opt-$d --label opt-$d --name opt-$d --config $config_file"
+	cmd="easeml create module --type $t --source upload --source-address $d --id opt-$d --label opt-$d --name opt-$d --config $config_file" 
 	echo $cmd
 	eval $cmd
 	cd ../..
     done
 done
 
-for t in "objective" "model"
+
+
+for t in "objective" "model"  
 do
     for d in `cat ./modules/${t}s/list`
     do
         echo "Install module [" $t "]: " $d
         cd ./modules/${t}s/$d
-        docker build -t $DUSER/$d .
-	#docker push $DUSER/$d
+        docker build -t $d .
         cd ..
-        cmd="easeml create module --type $t --source upload --source-address $DUSER/$d --id $d --label label-$d --name $d --config $config_file"
+        cmd="easeml create module --type $t --source upload --source-address $d --id $d --label label-$d --name $d --config $config_file"
         echo $cmd
         eval $cmd
         cd ../..
