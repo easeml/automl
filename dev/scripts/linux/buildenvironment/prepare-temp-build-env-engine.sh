ddae872b89b5e72e79e163af54855e65e2e771ea
Leonel Aguilar <leonel.aguilar.m@gmail.com>
2020-07-28 18:48:43 +0200

    Cleaned environment preparation

--- dev/scripts/linux/buildenvironment/prepare-temp-build-env-engine.sh
+++ dev/scripts/linux/buildenvironment/prepare-temp-build-env-engine.sh
@@ -4,11 +4,21 @@ echo "Updating"
 apt update
 echo "Preparing Build Environment"
 echo "Installing Make"
-apt install -y make
+if ! type make > /dev/null; then
+  apt install -y make
+fi
 echo "Installing git"
-apt install -y git
-apt install -y wget
-apt install -y curl
+if ! type git > /dev/null; then
+  apt install -y git
+fi
+echo "Installing wget"
+if ! type wget > /dev/null; then
+  apt install -y wget
+fi
+echo "Installing curl"
+if ! type curl > /dev/null; then
+  apt install -y curl
+fi
 echo "Getting NodeJS"
 export VERSION='12.8.0'
 export NBASE="node-v$VERSION-linux-x64.tar.gz"
@@ -17,10 +27,10 @@ export GBASE="go1.14.4.linux-amd64.tar.gz"
 export TOOL_INSTALL_PATH=/usr/local
 mkdir -p $TOOL_INSTALL_PATH
 wget https://nodejs.org/dist/v$VERSION/$NBASE
-tar -C $TOOL_INSTALL_PATH/ -xzf $NBASE --strip-components=1
+tar --overwrite -C $TOOL_INSTALL_PATH/ -xzf $NBASE --strip-components=1
 echo "Installing go"
 curl -O https://storage.googleapis.com/golang/$GBASE
-tar -C $TOOL_INSTALL_PATH/ -xzf $GBASE
+tar --overwrite -C $TOOL_INSTALL_PATH/ -xzf $GBASE
 mkdir -p ~/go
 echo "Cleaning"
 rm $NBASE
